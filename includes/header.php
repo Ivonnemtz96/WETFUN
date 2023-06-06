ce3632076d9006559f003b23609b9b8213a336b3
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-06-06 16:57:39 -0700

    direccion ok

--- includes/header.php
+++ includes/header.php
@@ -3,20 +3,23 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-4">
-                <a href="index.html" class="logo_normal"><img src="/img/logo_sticky.png?=1" width="135" height="65" alt=""></a>
-                <a href="index.html" class="logo_sticky"><img src="/img/logo_sticky.png?=1" width="135" height="65"
-                        alt=""></a>
+                <a href="/" class="logo_normal"><img src="/img/logo_sticky.png?=1" width="135" height="65" alt=""></a>
+                <a href="/" class="logo_sticky"><img src="/img/logo_sticky.png?=1" width="135" height="65" alt=""></a>
             </div>
             <div class="col-8">
                 <div class="main-menu">
                     <a href="#" class="closebt open_close_menu"><i class="bi bi-x"></i></a>
-                    <div class="logo_panel"><img src="/img/logo_sticky.png?=1" width="135" height="65" alt=""></div>
+                    <div class="logo_panel">
+                        <a href="/">
+                            <img src="/img/logo_sticky.png?=1" width="135" height="65" alt="">
+                        </a>
+                    </div>
                     <nav id="mainNav">
                         <ul>
                             <li><a href="/" class="animated_link">Inicio</a></li>
                             <li><a href="/galeria" class="animated_link">Galería</a></li>
-                            <li><a href="news-1.html" class="animated_link">¿Cómo llegar?</a></li>
-                            <li><a href="contacts.html" class="animated_link">Mapa</a></li>
+                            <li><a href="/direccion" class="animated_link">¿Cómo llegar?</a></li>
+                            <li><a href="/mapa" class="animated_link">Mapa</a></li>
                         </ul>
                     </nav>
                 </div>
