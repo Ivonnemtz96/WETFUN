7b239196e7a0a7a36b89eb52f5507bad7d2e551c
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-06-06 16:39:22 -0700

    Galería OK

--- includes/scripts.php
+++ includes/scripts.php
@@ -3,3 +3,16 @@
 <script src="/js/common_functions.js"></script>
 <script src="/js/datepicker_inline.js"></script>
 <script src="phpmailer/validate.js"></script>
+
+
+<!-- SPECIFIC SCRIPTS -->
+<script src="/js/isotope.min.js"></script>
+<script>
+    $(function() {
+        "use strict";
+        $(window).on('load', function() {
+            var $container = $('.isotope-wrapper');
+            $container.isotope({ itemSelector: '.item', layoutMode: 'masonry', });
+        });
+    });
+</script>
