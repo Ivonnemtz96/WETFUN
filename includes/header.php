e3a42bf5ede56361231680efea94e0613d04e76d
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-06-06 11:51:21 -0700

    se sube diseño al servidor

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<header class="fixed_header menu_v4">
+    <div class="layer"></div><!-- Opacity Mask -->
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-4">
+                <a href="index.html" class="logo_normal"><img src="img/logo.png" width="135" height="45" alt=""></a>
+                <a href="index.html" class="logo_sticky"><img src="img/logo_sticky.png" width="135" height="45"
+                        alt=""></a>
+            </div>
+            <div class="col-8">
+                <div class="main-menu">
+                    <a href="#" class="closebt open_close_menu"><i class="bi bi-x"></i></a>
+                    <div class="logo_panel"><img src="img/logo_sticky.png" width="135" height="45" alt=""></div>
+                    <nav id="mainNav">
+                        <ul>
+                            <li><a href="about.html" class="animated_link">About</a></li>
+                            <li><a href="room-list-1.html" class="animated_link">Rooms & Suites</a></li>
+                            <li><a href="news-1.html" class="animated_link">News & events</a></li>
+                            <li><a href="contacts.html" class="animated_link">Contacts</a></li>
+                            <li><a href="#booking_section" class="btn_1">Book Now</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="hamburger_2 open_close_menu float-end">
+                    <div class="hamburger__box">
+                        <div class="hamburger__inner"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- container -->
+</header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php
+    include("includes/head.php");
+?>
+
+<body class="datepicker_mobile_full">
+    <?php
+        include("includes/header.php");
+        include("modules/index.php");
+        include("includes/redes.php");
+        include("includes/footer.php");
+        include("includes/scripts.php");
+    ?>
+</body>
+
+</html>
